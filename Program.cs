b2e8c1110e2283e095fa17c2eff20997a207de73
Shubhalisonar <shubhali97@gmail.com>
2022-06-14 12:10:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display5Int
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                int[] ar = { 1, 2, 3, 4, 8 };
+
+                foreach (int m in ar)
+                {
+                    Console.WriteLine(m);
+                }
+                Console.ReadLine(); 
+        }
+    }
+    
+}
